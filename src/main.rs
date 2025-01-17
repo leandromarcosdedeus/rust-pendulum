c97fb0712eb4ce90fb0a2c97f0b18461d59de433
DevPrumo <leandromarcosdedeus@gmail.com>
2025-01-17 17:01:14 -0300

    pendulo incompleto

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+use crate::vector::Vector;
+
+use speedy2d::color::Color;
+use speedy2d::window::{WindowHandler, WindowHelper};
+use speedy2d::{Graphics2D, window};
+
 fn main() {
     println!("Hello, world!");
+}
+
+struct Pendulum {
+    origin: Vector,
+    position: Vector,
+    angle: f32,
+    angular_velocity: f32,
+    angular_acceleration: f32,
+    r: f32,
+    m: f32,
+    g: f32,
+}
+
+impl Pendulum {
+    fn new(x: f32, y: f32, r: f32) -> Pendulum {
+        Pendulum {
+            origin: Vector::new(x, y),
+            position: Vector::new(0.0, 0.0),
+            angle: 1.0,
+            angular_velocity: 0.0,
+            angular_acceleration: 0.0,
+            r,
+            m: 1.0,
+            g: 1.5,
+        }
+    }
+
+    fn update(&mut self) {
+        self.angular_acceleration = -1.0 * self.g * self.angle.sin() / self.r;
+        self.angular_velocity += self.angular_acceleration;
+        self.angle += self.angular_velocity;
+        self.position
+            .set(self.r * self.angle.sin(), self.r * self.angle.cos())
+            .add(&self.origin);
+    }
+
+    fn draw(&self) {
+        // Lógica para desenhar o pêndulo
+    }
+}
+
+mod vector {
+    pub struct Vector {
+        pub x: f32,
+        pub y: f32,
+    }
+
+    impl Vector {
+        pub fn new(x: f32, y: f32) -> Vector {
+            Vector { x, y }
+        }
+
+        pub fn add(&mut self, other: &Vector) -> &mut Vector {
+            self.x += other.x;
+            self.y += other.y;
+            self
+        }
+
+        pub fn set(&mut self, x: f32, y: f32) -> &mut Vector {
+            self.x = x;
+            self.y = y;
+            self
+        }
+    }
 }
